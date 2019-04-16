ad57153319149a1b07d294669cf0ff94deb0f4ea
陈 <574682566@qq.com>
2019-04-16 14:02:39 +0800

    longestCommonPrefix done

--- longest_common_prefix.js
+++ longest_common_prefix.js
@@ -1,58 +1,27 @@
 const log = console.log.bind(console)
 
-/**
- * @param {string[]} strs
- * @return {string}
- */
 const longestCommonPrefix = (strs) => {
     if (strs.length === 0) {
         return ''
     }
-    // 用最短的检测
-    strs.sort((a, b) => a.length - b.length)
-    let has = true
-    let r = ''
-    let first = strs[0]
-    while (first.length > 0) {
-        let i = 0
-        let char = first[i]
-        strs.forEach((s, index) => {
-            if (s.indexOf(char) === i) {
-                // 移除已经检测过的
-                let n = s.slice(i + 1)
-                strs[index] = n
-            } else {
-                has = false
+    let prefix = strs[0]
+    // 用第一个跟后面的比较
+    for (let i = 1; i < strs.length; i++) {
+        let item = strs[i]
+        let has = item.startsWith(prefix)
+        // 是否以 prefix 开头
+        while (!has) {
+            // 不是以 prefix 开头的话就去掉 prefix 的最后一个字符
+            prefix = prefix.substring(0, prefix.length - 1)
+            // 再判断
+            has = item.startsWith(prefix)
+            if (prefix.length === 0) {
+                return ""
             }
-        })
-        if (has) {
-            // 拼接已有的
-            r += char
-        }
-        // 移除已经检测过的
-        first = first.slice(i + 1)
-    }
-    for(let i = 0; i < first.length; i++) {
-        let char = first[i]
-        strs.forEach((s, index) => {
-            if (s.indexOf(char) === i) {
-                // 移除已经检测过的
-                let n = s.slice(i + 1)
-                strs[index] = n
-            } else {
-                has = false
-            }
-        })
-        if (has) {
-            // 拼接已有的
-            r += char
-            // 移除已经检测过的
-            first = first.slice(i + 1)
-            // 从头开始检测
-            i--
         }
     }
-    return r
+    return prefix
 }
 
-log(longestCommonPrefix(["flower","flow","fligh"]))
+log(longestCommonPrefix(["flower", "flow", "fligh"]))
+log(longestCommonPrefix(["aaa", "aaaa", "aaa"]))
